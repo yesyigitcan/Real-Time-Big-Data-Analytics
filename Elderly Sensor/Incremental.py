f9571f6251f4e254b2476ac06e5490ade4e1e4ff
yigitcan9797 <46279104+yigitcan9797@users.noreply.github.com>
2020-06-09 22:22:38 +0300

    Elderly sensor incremental logger added

--- Elderly Sensor/Incremental.py
+++ Elderly Sensor/Incremental.py
@@ -8,13 +8,22 @@ from creme.compose import Pipeline
 import time
 import matplotlib.pyplot as plt
 import requests
+import logging
+import logging.handlers
 
+logging.basicConfig(filename='ElderlyIncremental.log',
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%m/%d/%Y %H:%M:%S',
+                            level=logging.INFO)
+logging.info("Creating connection")
 engine = create_engine('mysql+pymysql://root:@localhost/tez')
-
-session = 41
+logging.info("Connection is ready")
+session = 60
 query = "select * from elderly_sensor where session = "+str(session)+" order by 'time(second)'"
 
 df = pandas.read_sql(query, engine)
+logging.info("Data retrieved by Session = " + str(session))
 df = df.drop("index", axis=1)
 timeList = list(df["time(second)"])
 
@@ -32,20 +41,24 @@ model = (
      StandardScaler() |
      DecisionTreeClassifier()
 )
-
+logging.info("Initial model created")
 # Mse Accuracy Real
-
-
+recordNumber = len(y)
 text = ""
+iterationTimeCounter = 0.0
 previous_time = 0.0
+logging.info("Learning process has been started")
 for row, target, time_passed in zip(x, y, timeList):
     time_range = time_passed - previous_time
     if time_range != 0.0:
         time.sleep(time_range)
+    iterationStartTime = time.time()
     y_pred = model.predict_one(row)
     model.fit_one(row, target)
     if y_pred is None:
         continue
+    iterationEndTime = time.time()
+    iterationTimeCounter += iterationEndTime - iterationStartTime
     for metric in metrics:
         metric.update(target, y_pred)
         print("%.5f" % metric.get(), end=" ")
@@ -57,9 +70,14 @@ for row, target, time_passed in zip(x, y, timeList):
         text += "0\n"
     print("Real:", target, " Predicted:", y_pred, " Time:", "%.5f" % time_passed, " Sleep Time:", "%.5f" % time_range)
     previous_time = time_passed
-    x = requests.get('http://localhost:7070/temp/4/incremental/' + str(metrics[0].get() * 100))
-    print(x.status_code)
-
+    #x = requests.get('http://localhost:7070/temp/4/incremental/' + str(metrics[0].get() * 100))
+    #print(x.status_code)
+logging.info("Learning process is done")
+logging.info("Total time for iterations in second " + str(iterationTimeCounter))
+logging.info("Average time for an iteration in second " + str(iterationTimeCounter / recordNumber))
+logging.info("Record number " + str(recordNumber))
+logging.info("Mean squared error " + str(metrics[0].get()))
+logging.info("Accuracy " + str(metrics[1].get()))
 try:
     outputfile = open('C:\\Users\\YigitCan\\Desktop\\Tez-Workspace\\Real-Time-Big-Data-Analytics\\Elderly Sensor\\Output'+str(session)+'.txt', 'w')
 except:
