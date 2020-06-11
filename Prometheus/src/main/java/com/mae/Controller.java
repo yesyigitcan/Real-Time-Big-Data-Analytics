67d9b44cb0b5d4b5e572e6e24c1aec4305b20c20
rebelsowl53@gmail.com <rebelsowl53@gmail.com>
2020-06-11 22:18:15 +0300

    ACC dasboards added

--- Prometheus/src/main/java/com/mae/Controller.java
+++ Prometheus/src/main/java/com/mae/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controller {
 
@@ -13,58 +14,90 @@ public class Controller {
     .name("temp1_4").help("help")
     .labelNames("approach").register();
 
+    static final Gauge temp1_4_acc = Gauge.build()  
+    .name("temp1_4_acc").help("help")
+    .labelNames("approach").register();
+
     static final Gauge temp1_2 = Gauge.build()  
     .name("temp1_2").help("help")
+    .labelNames("approach").register();
+
+    static final Gauge temp1_2_acc = Gauge.build()  
+    .name("temp1_2_acc").help("help")
     .labelNames("approach").register();
 
     static final Gauge temp = Gauge.build()  
     .name("temp").help("help")
     .labelNames("approach").register();
 
-    static final Gauge elderlySensor1_4 = Gauge.build()  
-    .name("elderlySensor1_4").help("help")
+    static final Gauge temp_acc = Gauge.build()  
+    .name("temp_acc").help("help")
     .labelNames("approach").register();
 
-    static final Gauge elderlySensor1_2 = Gauge.build()  
-    .name("elderlySensor1_2").help("help")
-    .labelNames("approach").register();
 
     static final Gauge elderlySensor = Gauge.build()  
     .name("elderlySensor").help("help")
     .labelNames("approach").register();
 
-    @RequestMapping(path= "temp/{size}/{approach}/{value}")
-    public String temp(@PathVariable String size,@PathVariable String approach, @PathVariable String value){
+    static final Gauge elderlySensor_acc = Gauge.build()  
+    .name("elderlySensor_acc").help("help")
+    .labelNames("approach").register();
+
+    @RequestMapping(path= "temp/{size}/{approach}/{m}/{value}")
+    public String temp(@PathVariable String size,@PathVariable String approach, @PathVariable String m, @PathVariable String value){
         Double d = Double.parseDouble(value);
+        boolean mse;
+        if (m.equals("mse")) {
+          mse = true;
+        } else if (m.equals("acc")) {
+          mse = false;
+        }else{
+          return( m + "tipinde bir metrik bulunmamaktadir");
+        }
 
         switch (Integer.parseInt(size)) {
             case 1:
-               temp.labels(approach).set(d);
+              if(mse){
+                temp.labels(approach).set(d);
+              }else{
+                temp_acc.labels(approach).set(d);
+              }
               break;
             case 2:
-              temp1_2.labels(approach).set(d);
+              if (mse) {
+                temp1_2.labels(approach).set(d);  
+              } else {
+                temp1_2_acc.labels(approach).set(d);
+              }
               break;
             case 4:
-              temp1_4.labels(approach).set(d);
+              if (mse) {
+                temp1_4.labels(approach).set(d);  
+              } else {
+                temp1_4_acc.labels(approach).set(d);
+              }
               break;
           }
         return d.toString();
     }
-    @RequestMapping(path= "elderlySensor/{size}/{approach}/{value}")
-    public String elderlySensor(@PathVariable String size,@PathVariable String approach, @PathVariable String value){
+    @RequestMapping(path= "elderlySensor/{size}/{approach}/{m}/{value}")
+    public String elderlySensor(@PathVariable String size,@PathVariable String approach,@PathVariable String m, @PathVariable String value){
         Double d = Double.parseDouble(value);
+        boolean mse;
+        if (m.equals("mse")) {
+          mse = true;
+        } else if (m.equals("acc")) {
+          mse = false;
+        }else{
+          return( m + "tipinde bir metrik bulunmamaktadir");
+        }
+        if(mse){
+          elderlySensor.labels(approach).set(d);
+        }else{
+          elderlySensor_acc.labels(approach).set(d);
+        }
+        
 
-        switch (Integer.parseInt(size)) {
-            case 1:
-               elderlySensor.labels(approach).set(d);
-              break;
-            case 2:
-              elderlySensor1_2.labels(approach).set(d);
-              break;
-            case 4:
-              elderlySensor1_4.labels(approach).set(d);
-              break;
-          }
         return d.toString();
     }
 
