f2495364a4845979c462e87f98e7ca4b6a8256ce
yigitcan9797 <46279104+yigitcan9797@users.noreply.github.com>
2020-06-14 03:47:11 +0300

    Activity Recognition Classical Approach Added

--- Activity Recognition/Classical.py
+++ Activity Recognition/Classical.py
@@ -1,2 +1,79 @@
-for i in range(0, 201, 100):
-    print(i)
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, accuracy_score, fbeta_score
+from sqlalchemy import create_engine
+import pandas
+import logging
+import time
+import requests
+
+logging.basicConfig(filename='ActivityRecogClassical.log',
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%m/%d/%Y %H:%M:%S',
+                            level=logging.INFO)
+
+n = 5
+logging.info("Creating connection")
+engine = create_engine('mysql+pymysql://root:@localhost/tez')
+logging.info("Connection is ready")
+
+limitForEachClass = 100
+logging.info("Limit for each class: " + str(limitForEachClass))
+classNames = ["bike", "null", "sit", "stairsdown", "stairup", "stand", "walk"]
+
+bigDf = None
+for i in range(len(classNames)):
+    query = "select x,y,z,gt from accelerometer a  where a.Model = 'nexus4' and a.gt = '" + classNames[i] + "' limit " + str(limitForEachClass)
+    df = pandas.read_sql(query, engine)
+    for i in range(1, n+1):
+        df["x_" + str(i)] = None
+        df["y_" + str(i)] = None
+        df["z_" + str(i)] = None
+    for i in range(n, len(df)):
+        for j in range(1, n+1):
+            df.loc[i, 'x_' + str(j)] = df.loc[i-j, 'x']
+            df.loc[i, 'y_' + str(j)] = df.loc[i-j, 'y']
+            df.loc[i, 'z_' + str(j)] = df.loc[i-j, 'z']
+    df = df.dropna(subset=["x_1"])
+    if type(bigDf) == None:
+        bigDf = df
+    else:
+        bigDf = pandas.concat([bigDf, df])
+logging.info("Big Df created")
+X = bigDf.drop("gt", axis=1)
+y = list(bigDf["gt"])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = DecisionTreeClassifier()
+logging.info("Initial model created")
+logging.info("Learning process has been started")
+learningStartTime = time.time()
+model.fit(X_train, y_train)
+learningEndTime = time.time()
+logging.info("Learning process is done")
+predictStartTime = time.time()
+predictStartTime = time.time()
+predict = model.predict(X_test)
+predictEndTime = time.time()
+acc = accuracy_score(y_test, predict)
+accRequest = requests.get('http://localhost:7070/elderlySensor/1/classical/acc/' +  str(acc * 100))    
+logging.info("Accuracy: " + str(acc))
+print("Accuracy: ", acc)
+
+try:
+    fbeta = fbeta_score(y_test, predict, average='macro', beta=0.5)
+    logging.info("Multiclass F Beta: " + str(fbeta))
+    print("Multiclass F Beta: ", fbeta)
+except Exception as e:
+    logging.info("Error|| " + str(e))
+
+
+
+
--- Activity Recognition/Incremental.py
+++ Activity Recognition/Incremental.py
@@ -1,7 +1,7 @@
 from creme.neighbors import KNeighborsClassifier
 from creme.tree import DecisionTreeClassifier
 from creme.preprocessing import StandardScaler
-from creme.metrics import MSE, Accuracy
+from creme.metrics import MSE,Accuracy, F1, LogLoss,MultiFBeta
 import pandas
 from sqlalchemy import create_engine
 from creme.compose import Pipeline
@@ -11,9 +11,11 @@ import requests
 import logging
 import logging.handlers
 import mysql.connector
+import tqdm
+
 dataPackageLimit = 100000
 tableName = "accelerometer"
-
+phoneType = "s3mini"
 
 db = mysql.connector.connect(
 	host="localhost",
@@ -24,13 +26,12 @@ db = mysql.connector.connect(
 
 
 cur = db.cursor()
-cur.execute("select count(*) from " + tableName)
+cur.execute("select count(*) from " + tableName + "_" + phoneType + " where Model = '" + phoneType + "'")
 tableSize = int(cur.fetchone()[0])
 cur.close()
 del db
 
 stepNumber = int(tableSize / dataPackageLimit)
-offsetNumber = tableSize - stepNumber * dataPackageLimit
 
 logging.basicConfig(filename='ActivityRecogIncremental.log',
                             filemode='a',
@@ -41,35 +42,38 @@ logging.info("Creating connection")
 engine = create_engine('mysql+pymysql://root:@localhost/tez')
 logging.info("Connection is ready")
 
-'''
+n = 5
 metrics = (
-    MSE(), 
-    Accuracy()
-    )
-'''
-metric = Accuracy()
+    Accuracy(),
+    MultiFBeta(betas=({'bike':1, 'null':1, 'sit':1, 'stairsdown':1, 'stairsup':1, 'stand':1, 'walk':1}))
+)
+
+
 
 model = (
      StandardScaler() |
      DecisionTreeClassifier()
 )
-logging.info("Initial model created")
+logging.info("Initial model created for phone type " + phoneType)
 
 #modelName = ["nexus4", "s3", "s3mini", "samsungold"]
 previousTime = None
-for step in range(stepNumber):
+logging.info("Learning stage started with total step of " + str(stepNumber))
+for step in tqdm.tqdm(range(stepNumber+1)):
     logging.info("Data retrieved at step " + str(step+1) + "/" + str(stepNumber))
-    query = "select * from " + tableName + " where Model = 'nexus4' and id >= " + str(step * dataPackageLimit) + " and id <= " + str((step+1) * dataPackageLimit)
+    if step < stepNumber:
+        query = "select * from " + tableName + "_" + phoneType + " where Model = '" + phoneType + "' and id >= " + str(step * dataPackageLimit) + " and id <= " + str((step+1) * dataPackageLimit)
+    else:
+        query = "select * from " + tableName + "_" + phoneType + " where Model = '" + phoneType + "' and id < " + str(stepNumber * dataPackageLimit)
     df = pandas.read_sql(query, engine)
     df = df.drop("id", axis=1)
     df = df.drop('Arrival_Time', axis=1)
     df = df.drop("Model", axis=1)
     x = df.drop("gt", axis=1).to_dict(orient="row")
     y = list(df["gt"])
-    
-    n = 5
+
     window = [] #  [ (x_1,y_1,z_1), (x_2,y_2,z_2), .... ]
-    for row, target in zip(x, y):
+    for row, target in tqdm.tqdm(zip(x, y)):
         if len(window) < n:
             window.append((row["x"], row["y"], row["z"]))
             continue
@@ -77,32 +81,73 @@ for step in range(stepNumber):
             row.update({"x_" + str(i): window[i][0]})
             row.update({"y_" + str(i): window[i][1]})
             row.update({"z_" + str(i): window[i][2]})
-        '''
-        for name in modelName:
-            if row["Model"] == name:
-                row.update({name:1})
-            else:
-                row.update({name:0})
-        row.pop("Model")
-        '''
 
         for i in range(n-1):
             window[i] = window[i+1]
         window[-1] = (row["x"], row["y"], row["z"])  
-        print(row)
+        
         y_pred = model.predict_one(row)
         model.fit_one(row, target)
         if y_pred is None:
             continue
-        '''
         for metric in metrics:
             metric.update(target, y_pred)
-            print(metric.get(), end=" ")
-        print()
-        '''
-        print("target: ", target, "  Pred: " , y_pred)
-        metric.update(target, y_pred)
-        print(metric.get())
+            #print(metric.get(), end=" ")
+        requests.get('http://localhost:7070/elderlySensor/1/incremental/acc/' + str(metrics[0].get() * 100))
+logging.info("Learning stage is done")
+logging.info("Test stage started")
+
+engine = create_engine('mysql+pymysql://root:@localhost/tez')
+logging.info("New connection for test stage established")
+
+limitForEachClass = 100
+classNames = ["bike", "null", "sit", "stairsdown", "stairup", "stand", "walk"]
+logging.info("Test stage has " + str(limitForEachClass) + " records for each class from " + str(len(classNames)) + " different classes")
+
+
+bigDf = None
+for i in range(len(classNames)):
+    query = "select x,y,z,gt from accelerometer a  where a.Model = '" + phoneType + "' and a.gt = '" + classNames[i] + "' limit " + str(limitForEachClass)
+    df = pandas.read_sql(query, engine)
+    for i in range(1, n+1):
+        df["x_" + str(i)] = None
+        df["y_" + str(i)] = None
+        df["z_" + str(i)] = None
+    for i in range(n, len(df)):
+        for j in range(1, n+1):
+            df.loc[i, 'x_' + str(j)] = df.loc[i-j, 'x']
+            df.loc[i, 'y_' + str(j)] = df.loc[i-j, 'y']
+            df.loc[i, 'z_' + str(j)] = df.loc[i-j, 'z']
+    df = df.dropna(subset=["x_1"])
+    if type(bigDf) == None:
+        bigDf = df
+    else:
+        bigDf = pandas.concat([bigDf, df])     
+logging.info("Test data set has been created")
+
+metrics = (
+    Accuracy(),
+    MultiFBeta(betas=({'bike':1, 'null':1, 'sit':1, 'stairsdown':1, 'stairsup':1, 'stand':1, 'walk':1}))
+)
+logging.info("Metrics are reset")
+bigDf = bigDf.reset_index()
+X = bigDf.drop("gt", axis=1)
+y = list(bigDf["gt"])
+
+logging.info("Test results have been started to send to Grafana")
+for i in range(len(y)):
+    row = X.iloc[i]
+    y_pred = model.predict_one(row)
+    y_real = y[i] 
+    if y_pred is None:
+        continue
+    for metric in metrics:
+        metric.update(y_real, y_pred)
+    requests.get('http://localhost:7070/elderlySensor/1/incremental/acc/' + str(metrics[0].get() * 100))
+logging.info("All processes are done")
+
+
+
 
     
 
